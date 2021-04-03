15b3e49ddf8574cc9207c31147e43ad556e9e90a
Dazikiri Anikar <mierzwinski.b@gmail.com>
2021-04-03 10:45:26 +0200

    The initial commit.

---
+++ src/test/java/bm/app/apigithubuser/ApiGithubUserApplicationTests.java
@@ -0,0 +1,13 @@
+package bm.app.apigithubuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGithubUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
